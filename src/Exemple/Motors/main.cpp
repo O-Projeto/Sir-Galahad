e0325ab737f87f1730c55ff8ca7440043f3c675b
Felix <raphaelfelix@alunos.utfpr.edu.br>
2023-09-16 15:32:22 -0300

    add exemple code for test motors

--- src/Exemple/Motors/main.cpp
+++ src/Exemple/Motors/main.cpp
@@ -1,56 +1,37 @@
-#include <Arduino.h>
-
-// Motors pin A
-#define AIN1 21
-#define AIN2 22
-
-// Motors pin B 
-#define BIN1 21
-#define BIN2 22
-
-const int freq = 1000; 
-
-const int channelA1 = 1;
-const int channelA2 = 2;
-const int channelB1 = 3;
-const int channelB2 = 4; 
-
-const int resolution = 8; 
+#include "Main/config.h"
 
 void setup() {
 
-    Serial.begin(9600);
+    pinMode(M1_IN1, OUTPUT);
+    pinMode(M1_IN2, OUTPUT); 
 
-    pinMode(AIN1, OUTPUT);
-    pinMode(AIN2, OUTPUT); 
+    pinMode(M2_IN1, OUTPUT); 
+    pinMode(M2_IN2, OUTPUT);
 
-    pinMode(BIN1, OUTPUT); 
-    pinMode(BIN2, OUTPUT);
+    ledcSetup(CHANNEL_M1_IN1, FREQUENCY, RESOLUTION); 
+    ledcSetup(CHANNEL_M1_IN2, FREQUENCY, RESOLUTION); 
 
-    ledcSetup(channelA1, freq, resolution); 
-    ledcSetup(channelA2, freq, resolution); 
+    ledcSetup(CHANNEL_M2_IN1, FREQUENCY, RESOLUTION);
+    ledcSetup(CHANNEL_M2_IN2, FREQUENCY, RESOLUTION);
 
-    ledcSetup(channelB1, freq, resolution);
-    ledcSetup(channelB2, freq, resolution);
+    ledcAttachPin(M1_IN1, CHANNEL_M1_IN1); 
+    ledcAttachPin(M1_IN2, CHANNEL_M1_IN2); 
 
-    ledcAttachPin(AIN1, channelA1); 
-    ledcAttachPin(AIN2, channelA2); 
-
-    ledcAttachPin(BIN1, channelB1); 
-    ledcAttachPin(BIN2, channelB2); 
+    ledcAttachPin(M2_IN1, CHANNEL_M2_IN1); 
+    ledcAttachPin(M1_IN2, CHANNEL_M2_IN2); 
 
 }
 
 void loop() {
 
     //PWM for the motors A and B 
-    digitalWrite(AIN1, LOW); 
-    digitalWrite(BIN1, LOW); 
+    digitalWrite(M1_IN1, LOW); 
+    digitalWrite(M1_IN2, LOW); 
 
     for(int vel=0; vel< 1000; vel++) {
 
-        ledcWrite(channelA2, vel); 
-        ledcWrite(channelB2, vel); 
+        ledcWrite(CHANNEL_M1_IN1, vel); 
+        ledcWrite(CHANNEL_M1_IN2, vel); 
         Serial.println(vel);
         delay(10);
     }
@@ -58,13 +39,13 @@ void loop() {
     delay(500);
 
     //PWM for the motors A and B 
-    digitalWrite(AIN2, LOW); 
-    digitalWrite(BIN2, LOW); 
+    digitalWrite(M2_IN1, LOW); 
+    digitalWrite(M2_IN2, LOW); 
 
     for(int vel=0; vel< 1000; vel++) {
 
-        ledcWrite(channelA1, vel); 
-        ledcWrite(channelB1, vel); 
+        ledcWrite(CHANNEL_M2_IN1, vel); 
+        ledcWrite(CHANNEL_M2_IN2, vel); 
         Serial.println(vel);
         delay(10);
     }
