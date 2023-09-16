1d3e0465538d920902761db9ab0b311c55113b3a
Felix <raphaelfelix@alunos.utfpr.edu.br>
2023-09-16 16:55:15 -0300

    add library and exemple code for reading imu sensor

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -5,11 +5,11 @@ motor motor_right(M1_IN1, M1_IN2, CHANNEL_M1_IN1, CHANNEL_M1_IN2);
 motor motor_left(M2_IN1, M2_IN2, CHANNEL_M2_IN1, CHANNEL_M2_IN2);
 
 void setup() {
-  motor_right.setup();
-  motor_left.setup();
+  // motor_right.setup();
+  // motor_left.setup();
 
-  motor_right.cmd(100);
-  motor_left.cmd(100);
+  // motor_right.cmd(100);
+  // motor_left.cmd(100);
 }
 
 void loop() {
